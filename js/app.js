8ea851f9e901f5feeda81b3bdd1b6c954bbee41e
emanuelhardwell <emanuelnoe16@gmail.com>
2020-11-03 10:24:33 -0600

    Finished Project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 /* 
 
 */
+
+window.src = ScrollReveal();
+
+src.reveal(".navbar", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "-100px",
+});
+
+src.reveal(".section1-left", {
+  duration: 3000,
+  origin: "left",
+  distance: "400px",
+});
+
+src.reveal(".section1-right", {
+  duration: 3000,
+  origin: "right",
+  distance: "400px",
+});
+
+src.reveal(".section3", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "400px",
+});
+
+src.reveal(".section4", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "400px",
+});
